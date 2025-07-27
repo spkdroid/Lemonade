2eaaecf49441ce47fbcbacddc21d235e30a8b61e
Ramkumar Velmurugan <vrk1990@outlook.com>
2025-07-27 08:36:25 -0400

    digital signing fixed

--- metro.config.js
+++ metro.config.js
@@ -1,11 +1,17 @@
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
 
-/**
- * Metro configuration
- * https://reactnative.dev/docs/metro
- *
- * @type {import('@react-native/metro-config').MetroConfig}
- */
-const config = {};
+const defaultConfig = getDefaultConfig(__dirname);
 
-module.exports = mergeConfig(getDefaultConfig(__dirname), config);
+const config = {
+  watchFolders: [],
+  resolver: {
+    blockList: [
+      // Exclude CMake temporary files
+      /.*\.cxx\/.*/,
+      /.*CMakeFiles\/.*/,
+      /.*CMakeTmp\/.*/,
+    ],
+  },
+};
+
+module.exports = mergeConfig(defaultConfig, config);
